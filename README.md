5e2a3c30e226344e15b8aa5e26cd1e30c822bdbd
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-05-18 16:16:36 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,42 @@
+# post-compile-webpack-plugin
+
+> Yo dawg!
+> I put a compiler in your compiler, so you can compile while you compile!
+
+This plugin enables you to start another compile step with access to the
+output of the main compilation.
+
+For example, if your current `webpack` setup generates `dist/lib.js`, with this
+plugin you can configure a child compilation step that executes after
+`dist/lib.js` has been built, so you can `require()` it.
+
+# Usage
+
+The plugin expects a `webpack` configuration object during initialization.
+Currently only `entry`, `output` and `plugins` are supported. In addition to
+that, the child compilation inherits the configuration of the main compilation.
+
+Example:
+
+```js
+const PostCompilePlugin = require( 'post-compile-webpack-plugin' );
+const path = require( 'path' );
+
+module.exports = {
+  entry: {
+    lib: './index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new PostCompilePlugin({
+      entry: {
+        example1: './examples/example1.js',
+        example2: './examples/example2.js'
+      }
+    })
+  ]
+};
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const EntryOptionPlugin = require( 'webpack/lib/EntryOptionPlugin' );
+const path = require( 'path' );
+const MemoryFS = require( 'memory-fs' );
+const OverlayInputFS = require( './lib/overlay-input-fs' );
+
+class PostCompilePlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    compiler.plugin('this-compilation', compilation => {
+      compilation.plugin('additional-assets', callback => {
+        const fs = new MemoryFS();
+        const compiler = this.createChildCompiler(compilation, fs);
+        const outputPath = compilation.compiler.outputPath;
+
+        Object.keys(compilation.assets).forEach(filename => {
+          const asset = compilation.assets[ filename ];
+          const filepath = path.join(outputPath, filename);
+          fs.mkdirpSync(path.dirname(filepath));
+          fs.writeFileSync(filepath, asset.source());
+        });
+
+        compiler.runAsChild(callback);
+      });
+    });
+  }
+
+  createChildCompiler(compilation, fs) {
+    const options = this.options;
+    const compiler = compilation.compiler;
+    const childCompiler = compilation.createChildCompiler('post-compile-webpack-plugin', options.output);
+    const notInOutput = filename => !fs.existsSync(filename);
+
+    childCompiler.inputFileSystem = new OverlayInputFS(fs, compiler.inputFileSystem);
+    childCompiler.outputFileSystem = compiler.outputFileSystem;
+    childCompiler.watchFileSystem = compiler.watchFileSystem;
+
+    if (options.plugins) {
+      options.plugins.forEach(plugin => { childCompiler.apply(plugin); });
+    }
+
+    if (options.entry) {
+      childCompiler._plugins['entry-option'] = null;
+      childCompiler.apply(new EntryOptionPlugin());
+      childCompiler.applyPluginsBailResult('entry-option', options.context, options.entry);
+    }
+
+    childCompiler.plugin('emit', (compilation, callback) => {
+      compilation.fileDependencies = compilation.fileDependencies.filter(notInOutput);
+      compilation.contextDependencies = compilation.contextDependencies.filter(notInOutput);
+      compilation.missingDependencies = compilation.missingDependencies.filter(notInOutput);
+      callback();
+    });
+
+    return childCompiler;
+  }
+}
+
+module.exports = PostCompilePlugin;
